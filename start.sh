b66c380156b728dab1a9997e27a0b5091f1f5c9b
izgnod <izgnod@gmail.com>
2016-05-13 17:51:25 +0800

    add

--- start.sh
+++ start.sh
@@ -8,4 +8,4 @@ else
     git pull
 fi
 
-docker run -it --rm --net=ci -v /var/run/docker.sock:/var/run/docker.sock -v /root/.ssh/:/root/.ssh/ -v $curDir/woci.json:/woci.json -v $curDir/17mei:/gopath/src/github.com/wothing/17mei  index.tenxcloud.com/izgnod/dockerci
+docker run -it --rm --net=test -v /var/run/docker.sock:/var/run/docker.sock -v /root/.ssh/:/root/.ssh/ -v $curDir/woci.json:/woci.json -v $curDir/17mei:/gopath/src/github.com/wothing/17mei  -v $curDir/bin/linux_64:/ci  index.tenxcloud.com/izgnod/dockerci
