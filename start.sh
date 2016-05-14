116c34167db4d16ea5ddaded352187d573b959ff
xiangdong.wu <xiangdong.wu@duitang.com>
2016-05-14 12:00:43 +0800

    add start

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ fi
 docker network create test 
 docker volume create --name=app
 
-docker run -it --rm --net=test -v /var/run/docker.sock:/var/run/docker.sock -v /root/.ssh/:/root/.ssh/ -v app:/app  -v $curDir/17mei:/gopath/src/github.com/wothing/17mei  -v $curDir/bin/linux_64:/ci  -v /root/.bashrc:/root/.bashrc -v app:/app index.tenxcloud.com/izgnod/dockerci
+docker run -it --rm --net=test -v /var/run/docker.sock:/var/run/docker.sock -v /root/.ssh/:/root/.ssh/ -v $curDir/17mei:/gopath/src/github.com/wothing/17mei  -v $curDir/bin/linux_64:/ci  -v /root/.bashrc:/root/.bashrc -v app:/app index.tenxcloud.com/izgnod/dockerci
