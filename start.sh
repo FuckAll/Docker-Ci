a0e70efaff84a632f49f7ecf8cdc9f451aff380e
xiangdong.wu <xiangdong.wu@duitang.com>
2016-05-14 10:47:01 +0800

    add start.sh

--- start.sh
+++ start.sh
@@ -10,4 +10,4 @@ fi
 
 docker network create test 
 
-docker run -it --rm --net=test -v /var/run/docker.sock:/var/run/docker.sock -v /root/.ssh/:/root/.ssh/ -v $curDir/woci.json:/woci.json -v $curDir/17mei:/gopath/src/github.com/wothing/17mei  -v $curDir/bin/linux_64:/ci  index.tenxcloud.com/izgnod/dockerci
+docker run -it --rm --net=test -v /var/run/docker.sock:/var/run/docker.sock -v /root/.ssh/:/root/.ssh/ -v $curDir/woci.json:/woci.json -v $curDir/17mei:/gopath/src/github.com/wothing/17mei  -v $curDir/bin/linux_64:/ci  -v /root/.bashrc:/root/.bashrc index.tenxcloud.com/izgnod/dockerci
