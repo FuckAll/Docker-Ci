7fcc37edbe242dca2d80a31f6df1dc6c3ae015e2
xiangdong.wu <xiangdong.wu@duitang.com>
2016-05-16 10:08:32 +0800

    add start

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ fi
 num=`docker network ls | awk '{print $2}' | grep test | wc -l`
 [ $num -ge 1 ] || docker network create test
 
-docker run -it --rm --net=test -v /var/run/docker.sock:/var/run/docker.sock -v /root/.ssh/:/root/.ssh/ -v $curDir/17mei:/gopath/src/github.com/wothing/17mei -v /root/.bashrc:/root/.bashrc -v app:/app -v log:/log/ -v $curDir/hooks.json:/hooks.json -v $curDir/webhook.sh:/webhook.sh -v $curDir/woci.json:/woci.json -v $curDir/buildlog.exe:/buildlog.exe  -p 9090:9090 -p 9000:9000 index.tenxcloud.com/izgnod/dockercia
+docker run -it --rm --net=test -v /var/run/docker.sock:/var/run/docker.sock -v /root/.ssh/:/root/.ssh/ -v $curDir/17mei:/gopath/src/github.com/wothing/17mei -v /root/.bashrc:/root/.bashrc -v app:/app -v log:/log/ -v $curDir/hooks.json:/hooks.json -v $curDir/webhook.sh:/webhook.sh -v $curDir/woci.json:/woci.json -v $curDir/buildlog.exe:/buildlog.exe  -p 9090:9090 -p 9000:9000 index.tenxcloud.com/izgnod/dockerci
